737f2228e2a5ceb9bb32ff6785032b3250f99198
dmahoney <p!ttp@nth3r>
2016-11-07 11:10:31 -0500

    adding meeting seach ejs

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log('Ready to go!');
     $("#sidebar [href]").click(function(event) {
-        var id = $(this).attr('id'); 
+        var id = $(this).attr('id');
         console.log(id);
         $.get('http://localhost:5000/getDayOfWeek', { data: id }, function(data) {
 
